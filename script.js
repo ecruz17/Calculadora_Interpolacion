6b5a3aae2d222fa08f4e8587766dabf4cc3996ae
ecruz17 <ecruz17@ucol.mx>
2021-04-15 20:43:34 -0500

    added all files

---
+++ script.js
@@ -0,0 +1,51 @@
+export default class App {
+
+//Formulas de interpolación
+interpolacionLineal(x,x0,fx0,x1,fx1){
+    let fxinterpolado= fx0 + ((fx1-fx0)/x1-x0)(x-x0);
+    return fxinterpolado;
+
+}
+
+obtenerError(fxreal,fxinterpolado){
+    if(fxreal!==0){
+        let errorVerdadero= fxinterpolado-fxreal;
+        let errorRelativoPorcentual= (errorVerdadero/fxreal)(100);
+        return errorRelativoPorcentual;
+    }
+}
+
+interpolacionCuadratica(x,x0,fx0,x1,fx1,x2,fx2){
+    let b0=fx0;
+    let b1=(fx1-fx0)/(x1-x0);
+    let b2=(((fx2-fx1)/x2-x1)-((fx1-fx0)/x1-x0))/(x2-x0);
+    return b0+(b1)(x-x0)+(b2)(x-x0)(x-x1);
+}
+
+interpolacionLagrange1erOrden(x,x0,fx0,x1,fx1){
+    return ((x-x1)/(x0-x1))(fx0)+((x-x0)/(x1-x0))(fx1);
+}
+
+interpolacionLagrange2doOrden(x,x0,fx0,x1,fx1,x2,fx2){
+    return ((x-x1/x0-x1)(x-x2/x0-x2))(fx0)+((x-x0/x1-x0)(x-x2/x1-x2))(fx1)+((x-x0/x2-x0)(x-x1/x2-x1))(fx2);
+}
+
+
+
+}
+
+
+
+let app = new App();
+
+//x,x0,fx0,x1,fx1
+
+console.log(interpolacionLineal(6,0,5,14,7,22));
+
+/*Animacion de carga
+window.addEventListener("load", function() {
+    const LOADER = document.querySelector(".loader");
+    LOADER.className += ' hidden';
+    });
+    */
+
---
+++ scripts/interpolacion-cuadratica.js
@@ -0,0 +1,76 @@
+function calcularCuadratica() {
+    
+    var x0=parseFloat(document.getElementById('x0c').value);
+    var fx0=parseFloat(document.getElementById('fx0c').value);
+    var x1=parseFloat(document.getElementById('x1c').value);
+    var fx1=parseFloat(document.getElementById('fx1c').value);
+    var x2=parseFloat(document.getElementById('x2c').value);
+    var fx2=parseFloat(document.getElementById('fx2c').value);
+    var x=parseFloat(document.getElementById('xc').value);
+    var fx=parseFloat(document.getElementById('fxc').value);
+    var decimales=parseFloat(document.getElementById('decimales1').value);  
+
+    if(decimales !== decimales) {
+        decimales = 3;
+    }
+
+    fx = fx0+((fx1-fx0)/(x1-x0))*(x-x0)+((((fx2-fx1)/x2-x1)-((fx1-fx0)/x1-x0))/(x2-x0))*(x-x0)*(x-x1);
+
+    
+
+    document.getElementById('fxc').value=fx;
+    
+    let res = this.truncateDecimals(fx, decimales);
+    document.getElementById('fxc').value=res; 
+
+         
+}
+
+function reiniciarCuadratica() {
+    document.getElementById('x0c').value='';
+    document.getElementById('fx0c').value='';
+    document.getElementById('x1c').value='';
+    document.getElementById('fx1c').value='';
+    document.getElementById('x2c').value='';
+    document.getElementById('fx2c').value='';
+    document.getElementById('fxc').value='';
+    document.getElementById('xc').value='';
+    document.getElementById('decimales1').value='';
+    document.getElementById('errorPorcentual').value='';
+    document.getElementById('valorReal1').value='';
+    document.getElementById('res1').value='';
+} 
+
+
+truncateDecimals = function (number, digits) {
+
+    var multiplier = Math.pow(10, digits),
+    adjustedNum = number * multiplier,
+    truncatedNum = Math[adjustedNum < 0 ? 'ceil' : 'floor'](adjustedNum);
+    
+    return truncatedNum / multiplier;
+    
+};
+
+function calcularErrorActivate() {
+    document.getElementById('check').onchange = function() {
+        document.getElementById('valorReal1').disabled = !this.checked;
+        document.getElementById('btnErrorP1').disabled = !this.checked;
+        document.getElementById('res1').disabled = !this.checked;
+    };
+    
+}
+
+
+function calcularErrorL() {
+
+    var errorP=parseFloat(document.getElementById('errorPorcentual').value);
+    var ep=parseFloat(document.getElementById('valorReal1').value);
+    var resFinal=parseFloat(document.getElementById('res1').value);
+
+    errorP = ((resFinal-ep)/ep)*100;
+    
+
+    document.getElementById('errorPorcentual').value=`${errorP}%`;
+    
+}
